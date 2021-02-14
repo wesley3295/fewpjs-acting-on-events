0513f2a696db3375186ce5ddd3ae41b3f1da0c8a
Wesley <wspencer3295@yahoo.com>
2021-02-13 21:59:19 -0700

    commit tree 21e35858f3ba9bd9100068753b1431fd4b38ef7f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+let dodger = document.getElementById("dodger"); 
+dodger.style.backgroundColor = "#FF69B4"; 
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+} 
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+    
+    if (right > 0) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+       moveDodgerLeft()
+      }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+      }
+  }); 
